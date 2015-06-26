37225a9a0c2eac7cf4d2c80d3927eb88b195c61a
marcin walendzik <marcin.walendzik@gmail.com>
2015-06-27 00:29:54 +0200

    nice conversion to hash done

--- app/report.rb
+++ app/report.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Report
 
   PATTERNS = {:route => /add route \D+ \D+ \d+ \d+ \d+/,
@@ -6,6 +7,15 @@ class Report
              :airline_passanger => /add airline \D+ \d{1,3}/,
              :loyalty_passanger => /add loyalty \D+ \d{1,3} \d+ (TRUE|FALSE) (TRUE|FALSE)/}
 
+  ROUTE_KEYS = [:origin,:destination,
+                :cost_pp,:price_pp,
+                :min_pct]
+  ROUTE_CONV = [:to_s,:to_s,
+                :to_f,:to_f,
+                :to_f]  
+
+  AIRCRAFT_KEYS = [:name,:seats_number]
+  AIRCRAFT_CONV = [:to_s,:to_f]           
          
 
   attr_reader :input_file,:output_file,:extract,:errors           
@@ -35,10 +45,18 @@ class Report
   end
 
   def extract_data
-    # read_inputs
     @extract = load_file(input_file,PATTERNS)
     set_error("Wrong number of routes") if @extract[:route].size != 1
     set_error("Wrong number of aircrafts") if @extract[:aircraft].size != 1
+  end
+
+  #reads given data and converts it into hash/dictionary
+  def convert_to_hash(symbol,start_from=2,keys,conv)
+    result = {}
+    @extract[symbol][0].split(' ')[start_from..-1].each_with_index do |el,ix|
+      result[keys[ix]] = el.send(conv[ix])
+    end
+    result
   end
 
 
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -13,13 +13,38 @@ describe Report do
     expect(report.extract[:general_passanger].length).to eq(3)
     expect(report.extract[:airline_passanger].length).to eq(1)
     expect(report.extract[:loyalty_passanger].length).to eq(1)
-    p report.extract[:route] 
   end
 
   it 'should set error' do 
     report.set_files("./sample3.txt")
     report.extract_data
     expect(report.errors[0]).to eq("Wrong number of routes")
+  end 
+
+  it 'should extract the route' do
+    ROUTE_KEYS = [:origin,:destination,
+                :cost_pp,:price_pp,
+                :min_pct]
+    ROUTE_CONV = [:to_s,:to_s,
+                :to_f,:to_f,
+                :to_f]  
+    report.set_files("./sample1.txt")
+    report.extract_data
+    expect(report.convert_to_hash(:route,2,ROUTE_KEYS,ROUTE_CONV)).to eq({:origin=>"London",
+                                                                          :destination=>"Dublin",
+                                                                          :cost_pp=>100,
+                                                                          :price_pp=>150,
+                                                                          :min_pct=>75})
+  end
+
+  it 'should extract the aircraft' do 
+
+    AIRCRAFT_KEYS = [:name,:seats_number]
+    AIRCRAFT_CONV = [:to_s,:to_f]  
+    report.set_files("./sample1.txt")
+    report.extract_data
+    expect(report.convert_to_hash(:aircraft,2,AIRCRAFT_KEYS,AIRCRAFT_CONV)).to eq({:name=>"Gulfstream-G550",
+                                                                          :seats_number=>8.0})
   end 
 
 end
